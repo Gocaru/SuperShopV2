69bc0023a5ac97048ffed3cf279c1bd7a70d64de
Goncalo Russo <goncalorusso@gmail.com>
2025-05-16 15:42:38 +0100

    First Controller

---
+++ SuperShopV2/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperShopV2.Data.Entities;
+
+namespace SuperShopV2.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }    //Propriedade responsável pela tabela
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+    }
+}
